1184079d970646ce3f830dea5612027a4da9d8a6
Tatiana Cuadros <tatianacuadros31@gmail.com>
2023-10-25 18:48:12 -0500

    Modelo y BD para ponentes

--- controllers/PonentesController.php
+++ controllers/PonentesController.php
@@ -3,6 +3,8 @@
 namespace Controllers;
 
 use MVC\Router;
+use Model\Ponente;
+use Intervention\Image\ImageManagerStatic as Image;
 
 class PonentesController {
 
@@ -19,57 +21,57 @@ class PonentesController {
         // }
 
         $alertas = [];
-        // $ponente = new Ponente;
+        $ponente = new Ponente();
 
-        // if($_SERVER['REQUEST_METHOD'] === 'POST') {
+        if($_SERVER['REQUEST_METHOD'] === 'POST') {
         //     if(!is_admin()) {
         //         header('Location: /login');
         //     }
 
-        //     // Leer imagen
-        //     if(!empty($_FILES['imagen']['tmp_name'])) {
+            //Leer imagen
+            if(!empty($_FILES['imagen']['tmp_name'])) {
                 
-        //         $carpeta_imagenes = '../public/img/speakers';
+                $carpeta_imagenes = '../public/img/speakers';
 
-        //         // Crear la carpeta si no existe
-        //         if(!is_dir($carpeta_imagenes)) {
-        //             mkdir($carpeta_imagenes, 0755, true);
-        //         }
+                //Crear la carpeta si no existe
+                if(!is_dir($carpeta_imagenes)) {
+                    mkdir($carpeta_imagenes, 0755, true);
+                }
 
-        //         $imagen_png = Image::make($_FILES['imagen']['tmp_name'])->fit(800,800)->encode('png', 80);
-        //         $imagen_webp = Image::make($_FILES['imagen']['tmp_name'])->fit(800,800)->encode('webp', 80);
+                $imagen_png = Image::make($_FILES['imagen']['tmp_name'])->fit(800,800)->encode('png', 80);
+                $imagen_webp = Image::make($_FILES['imagen']['tmp_name'])->fit(800,800)->encode('webp', 80);
 
-        //         $nombre_imagen = md5( uniqid( rand(), true) );
+                $nombre_imagen = md5( uniqid( rand(), true) ); //Genra un nombre aleatorio
 
-        //         $_POST['imagen'] = $nombre_imagen;
-        //     } 
-        //     $_POST['redes'] = json_encode( $_POST['redes'], JSON_UNESCAPED_SLASHES );        
-        //     $ponente->sincronizar($_POST);
+                $_POST['imagen'] = $nombre_imagen;
+            } 
+            $_POST['redes'] = json_encode( $_POST['redes'], JSON_UNESCAPED_SLASHES );        
+            $ponente->sincronizar($_POST);
 
-        //     // validar
-        //     $alertas = $ponente->validar();
+            // validar
+            $alertas = $ponente->validar();
 
 
-        //     // Guardar el registro
-        //     if(empty($alertas)) {
+            //Guardar el registro
+            if(empty($alertas)) {
 
-        //         // Guardar las imagenes
-        //         $imagen_png->save($carpeta_imagenes . '/' . $nombre_imagen . ".png" );
-        //         $imagen_webp->save($carpeta_imagenes . '/' . $nombre_imagen . ".webp" );
+                //Guardar las imagenes
+                $imagen_png->save($carpeta_imagenes . '/' . $nombre_imagen . ".png" );
+                $imagen_webp->save($carpeta_imagenes . '/' . $nombre_imagen . ".webp" );
 
-        //         // Guardar en la BD
-        //         $resultado = $ponente->guardar();
+                //Guardar en la BD
+                $resultado = $ponente->guardar();
 
-        //         if($resultado) {
-        //             header('Location: /admin/ponentes');
-        //         }
-        //     }
-        // }
+                if($resultado) {
+                    header('Location: /admin/ponentes');
+                }
+            }
+        }
 
         $router->render('admin/ponentes/crear', [
             'titulo' => 'Registrar Ponente',
-            'alertas' => $alertas
-            // 'ponente' => $ponente,
+            'alertas' => $alertas,
+            'ponente' => $ponente
             // 'redes' => json_decode($ponente->redes)
         ]);
     }
