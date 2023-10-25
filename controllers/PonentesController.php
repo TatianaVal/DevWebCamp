01fc17103c4f4d78bf2c38b30d78510ea7b0c126
Tatiana Cuadros <tatianacuadros31@gmail.com>
2023-10-25 16:51:21 -0500

    Formulario de Registro, Ponenetes

--- controllers/PonentesController.php
+++ controllers/PonentesController.php
@@ -12,4 +12,65 @@ class PonentesController {
             'titulo' => 'Ponentes / Conferencistas',
         ]);
     }
+
+    public static function crear(Router $router) {
+        // if(!is_admin()) {
+        //     header('Location: /login');
+        // }
+
+        $alertas = [];
+        // $ponente = new Ponente;
+
+        // if($_SERVER['REQUEST_METHOD'] === 'POST') {
+        //     if(!is_admin()) {
+        //         header('Location: /login');
+        //     }
+
+        //     // Leer imagen
+        //     if(!empty($_FILES['imagen']['tmp_name'])) {
+                
+        //         $carpeta_imagenes = '../public/img/speakers';
+
+        //         // Crear la carpeta si no existe
+        //         if(!is_dir($carpeta_imagenes)) {
+        //             mkdir($carpeta_imagenes, 0755, true);
+        //         }
+
+        //         $imagen_png = Image::make($_FILES['imagen']['tmp_name'])->fit(800,800)->encode('png', 80);
+        //         $imagen_webp = Image::make($_FILES['imagen']['tmp_name'])->fit(800,800)->encode('webp', 80);
+
+        //         $nombre_imagen = md5( uniqid( rand(), true) );
+
+        //         $_POST['imagen'] = $nombre_imagen;
+        //     } 
+        //     $_POST['redes'] = json_encode( $_POST['redes'], JSON_UNESCAPED_SLASHES );        
+        //     $ponente->sincronizar($_POST);
+
+        //     // validar
+        //     $alertas = $ponente->validar();
+
+
+        //     // Guardar el registro
+        //     if(empty($alertas)) {
+
+        //         // Guardar las imagenes
+        //         $imagen_png->save($carpeta_imagenes . '/' . $nombre_imagen . ".png" );
+        //         $imagen_webp->save($carpeta_imagenes . '/' . $nombre_imagen . ".webp" );
+
+        //         // Guardar en la BD
+        //         $resultado = $ponente->guardar();
+
+        //         if($resultado) {
+        //             header('Location: /admin/ponentes');
+        //         }
+        //     }
+        // }
+
+        $router->render('admin/ponentes/crear', [
+            'titulo' => 'Registrar Ponente',
+            'alertas' => $alertas
+            // 'ponente' => $ponente,
+            // 'redes' => json_decode($ponente->redes)
+        ]);
+    }
 }
